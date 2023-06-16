16435652d890f7ad616125bfec7977c00bc199eb
Gabriel Scarlat <gabrielscarlat36@gmail.com>
2023-06-16 16:25:21 +0200

    sistemato MommessaServiceImpl

--- src/main/java/it/prova/cogebe/service/CommessaServiceImpl.java
+++ src/main/java/it/prova/cogebe/service/CommessaServiceImpl.java
@@ -10,45 +10,47 @@ import it.prova.cogebe.model.Commessa;
 import it.prova.cogebe.repository.CommessaRepository;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 public class CommessaServiceImpl implements CommessaService {
 
-    @Autowired
-    private CommessaRepository repository;
+	@Autowired
+	private CommessaRepository repository;
 
-    @Override
-    @Transactional(readOnly = true)
-    public List<Commessa> listAll() throws Exception {
-        return (List<Commessa>) repository.findAll();
-    }
+	@Override
+	public List<Commessa> listAll() throws Exception {
+		return (List<Commessa>) repository.findAll();
+	}
 
-    @Override
-    @Transactional(readOnly = true)
-    public Commessa caricaSingoloElemento(Long id) throws Exception {
-        return repository.findById(id).orElse(null);
-    }
+	@Override
+	public Commessa caricaSingoloElemento(Long id) throws Exception {
+		return repository.findById(id).orElse(null);
+	}
 
-    @Override
-    public Commessa aggiorna(Commessa commessaInstance) throws Exception {
-        if (commessaInstance == null || commessaInstance.getId() == null || caricaSingoloElemento(commessaInstance.getId()) == null) {
-            throw new Exception("Impossibile aggiornare la commessa.");
-        }
-        return repository.save(commessaInstance);
-    }
+	@Transactional
+	@Override
+	public Commessa aggiorna(Commessa commessaInstance) throws Exception {
+		if (commessaInstance == null || commessaInstance.getId() == null
+				|| caricaSingoloElemento(commessaInstance.getId()) == null) {
+			throw new Exception("Impossibile aggiornare la commessa.");
+		}
+		return repository.save(commessaInstance);
+	}
 
-    @Override
-    public Commessa inserisciNuovo(Commessa commessaInstance) throws Exception {
-        if (commessaInstance == null) {
-            throw new Exception("Impossibile inserire una commessa nulla.");
-        }
-        return repository.save(commessaInstance);
-    }
+	@Transactional
+	@Override
+	public Commessa inserisciNuovo(Commessa commessaInstance) throws Exception {
+		if (commessaInstance == null) {
+			throw new Exception("Impossibile inserire una commessa nulla.");
+		}
+		return repository.save(commessaInstance);
+	}
 
-    @Override
-    public void rimuovi(Long id) throws Exception {
-        if (id == null || caricaSingoloElemento(id) == null) {
-            throw new Exception("Impossibile rimuovere la commessa.");
-        }
-        repository.deleteById(id);
-    }
+	@Transactional
+	@Override
+	public void rimuovi(Long id) throws Exception {
+		if (id == null || caricaSingoloElemento(id) == null) {
+			throw new Exception("Impossibile rimuovere la commessa.");
+		}
+		repository.deleteById(id);
+	}
 }
