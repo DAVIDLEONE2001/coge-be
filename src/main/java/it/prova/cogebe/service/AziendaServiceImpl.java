828130e2e9c52540d215ff92ca7a067e264330bb
CristianPiarulli <piarullicristian97@gmail.com>
2023-06-16 16:22:06 +0200

    Sistemati errori service

--- src/main/java/it/prova/cogebe/service/AziendaServiceImpl.java
+++ src/main/java/it/prova/cogebe/service/AziendaServiceImpl.java
@@ -10,24 +10,23 @@ import it.prova.cogebe.model.Azienda;
 import it.prova.cogebe.repository.AziendaRepository;
 
 @Service
+@Transactional(readOnly = true)
 public class AziendaServiceImpl implements AziendaService {
 	
 	@Autowired
 	private AziendaRepository repository;
 
-	@Transactional(readOnly = true)
+	
 	@Override
 	public List<Azienda> listAll() throws Exception {
 		return (List<Azienda>) repository.findAll();		
 	}
 
-	@Transactional(readOnly = true)
+	@Transactional
 	@Override
 	public Azienda caricaSingoloElemento(Long id) throws Exception {
 		return repository.findById(id).orElse(null);
 	}
-
-	@Transactional
 	@Override
 	public Azienda aggiorna(Azienda aziendaInstance) throws Exception {
 		if (caricaSingoloElemento(aziendaInstance.getId()) != null) {
