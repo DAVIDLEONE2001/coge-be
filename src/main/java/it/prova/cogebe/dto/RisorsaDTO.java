90b07b74f131386f64655224e743546f5fbef9cc
DAVIDLEONE2001 <david.leone.2001@gmailcom>
2023-06-16 12:36:02 +0200

    RisorsaDTO in attesa dei vistri DTO

--- src/main/java/it/prova/cogebe/dto/RisorsaDTO.java
+++ src/main/java/it/prova/cogebe/dto/RisorsaDTO.java
@@ -4,6 +4,13 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.persistence.FetchType;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+
+import it.prova.cogebe.model.Attachment;
+import it.prova.cogebe.model.Commessa;
+import it.prova.cogebe.model.Rapportino;
 import it.prova.cogebe.model.Risorsa;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -24,13 +31,19 @@ public class RisorsaDTO {
 	private String codiceFiscale;
 	private String email;
 	private Integer costoGiornaliero;
+	private AttachmentDTO cv;
 
+	private List<Commessa> commesse;
+
+//	qua servono i vostri DTO
+	private List<Rapportino> rapportini;
 	public static RisorsaDTO buildRisorsaDTOFromModel(Risorsa risorsaModel) {
 
 		RisorsaDTO result = RisorsaDTO.builder().id(risorsaModel.getId()).nome(risorsaModel.getNome())
 				.cognome(risorsaModel.getCognome()).dataIn(risorsaModel.getDataIn()).dataOut(risorsaModel.getDataOut())
 				.codiceFiscale(risorsaModel.getCodiceFiscale()).email(risorsaModel.getEmail())
-				.costoGiornaliero(risorsaModel.getCostoGiornaliero()).build();
+				.costoGiornaliero(risorsaModel.getCostoGiornaliero())
+				.cv(AttachmentDTO.buildAttachmentDTOFromModel(risorsaModel.getCv())).build();
 		return result;
 	}
 
@@ -42,10 +55,9 @@ public class RisorsaDTO {
 	}
 
 	public Risorsa buildRisorsaModel() {
-		Risorsa result = Risorsa.builder().id(this.id).nome(this.nome).cognome(this.cognome)
-				.dataIn(this.dataIn).dataOut(this.dataOut)
-				.codiceFiscale(this.codiceFiscale).email(this.email)
-				.costoGiornaliero(this.costoGiornaliero).build();
+		Risorsa result = Risorsa.builder().id(this.id).nome(this.nome).cognome(this.cognome).dataIn(this.dataIn)
+				.dataOut(this.dataOut).codiceFiscale(this.codiceFiscale).email(this.email)
+				.costoGiornaliero(this.costoGiornaliero).cv(this.cv.buildAttachmentModel()).build();
 
 		return result;
 	}
